48a6357eeccfe532836fcb93d65240ff0823f8c9
Alex Kir <alex@wall-dev.org>
2017-11-10 03:27:00 +0800

    Added documentation strings

--- errlog.go
+++ errlog.go
@@ -4,26 +4,32 @@ import (
 	"github.com/Sirupsen/logrus"
 )
 
+// Panic with given error
 func Err_panic(err error) {
 	Err_err(err, logrus.Panicf)
 }
 
+// Output critical with given error
 func Err_critical(err error) {
 	Err_err(err, logrus.Errorf)
 }
 
+// Output warning with given error
 func Err_warning(err error) {
 	Err_err(err, logrus.Warnf)
 }
 
+// Output info with given error
 func Err_info(err error) {
 	Err_err(err, logrus.Infof)
 }
 
+// Output debug with given error
 func Err_debug(err error) {
 	Err_err(err, logrus.Debugf)
 }
 
+// Output err with given error
 func Err_err(err error, cb func(string, ...interface{})) error {
 	if err != nil {
 		if cb != nil {
