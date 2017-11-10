2ae532f50ca2cbfc9964db4539fb9314bbd74f4e
Alex Kir <alex@wall-dev.org>
2017-11-10 22:06:42 +0800

    Fixes

--- errlog.go
+++ errlog.go
@@ -4,33 +4,33 @@ import (
 	"github.com/Sirupsen/logrus"
 )
 
-// Panic with given error
-func Err_panic(err error) {
-	Err_err(err, logrus.Panicf)
+// ErrPanic Panic with given error
+func ErrPanic(err error) {
+	ErrErr(err, logrus.Panicf)
 }
 
-// Output critical with given error
-func Err_critical(err error) {
-	Err_err(err, logrus.Errorf)
+// ErrCritical Output critical with given error
+func ErrCritical(err error) {
+	ErrErr(err, logrus.Errorf)
 }
 
-// Output warning with given error
-func Err_warning(err error) {
-	Err_err(err, logrus.Warnf)
+// ErrWarning Output warning with given error
+func ErrWarning(err error) {
+	ErrErr(err, logrus.Warnf)
 }
 
-// Output info with given error
-func Err_info(err error) {
-	Err_err(err, logrus.Infof)
+// ErrInfo Output info with given error
+func ErrInfo(err error) {
+	ErrErr(err, logrus.Infof)
 }
 
-// Output debug with given error
-func Err_debug(err error) {
-	Err_err(err, logrus.Debugf)
+// ErrDebug Output debug with given error
+func ErrDebug(err error) {
+	ErrErr(err, logrus.Debugf)
 }
 
-// Output err with given error
-func Err_err(err error, cb func(string, ...interface{})) error {
+// ErrErr Output err with given error
+func ErrErr(err error, cb func(string, ...interface{})) error {
 	if err != nil {
 		if cb != nil {
 			cb("%s", err.Error())
